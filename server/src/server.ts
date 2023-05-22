8669b36f93b3bdb9a5df638ca1573f1d0728f431
Daniel dos Passos <daniel.passos@qsaude.com.br>
2023-05-21 23:43:12 -0300

    feat: integrate with project web and mobile

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,22 @@ import 'dotenv/config'
 
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import multipart from '@fastify/multipart'
 import jwt from '@fastify/jwt'
+import { resolve } from 'node:path'
+
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true,
@@ -18,11 +29,12 @@ app.register(jwt, {
 
 app.register(authRoutes)
 app.register(memoriesRoutes)
+app.register(uploadRoutes)
 
 app
   .listen({
     port: 3333,
-    host: '0.0.0.0',
+    // host: '0.0.0.0',
   })
   .then(() => {
     console.log('HTTP server running on port http://localhost:3333')
